9121d242e843979bab1a56796de5b2ea0a68f3e3
Eric Brooke <ericbrooke@mac.com>
2016-07-24 14:58:32 -0500

    model layer

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?",
+                               "What is 7+7?",
+                               "What is the capital of Canada?"]
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Ottawa"]
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject){
         
     }
