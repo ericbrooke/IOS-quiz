d638895610feed393c71ae9575a69e02867716b7
Eric Brooke <ericbrooke@mac.com>
2016-08-13 16:24:42 -0500

    WIP Animation off side screen function, not fun. constraints

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -46,19 +46,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
+    }
+    
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
     }
     
     func animateLabelTransitions() {
-
+        
+        view.layoutIfNeeded()
+        
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
         UIView.animateWithDuration(0.5,
                                    delay: 0,
                                    options: [],
             animations: {
             self.currentQuestionLabel.alpha = 0
             self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded()
         },
             completion: { _ in
                 swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                self.updateOffScreenLabel()
         })
     }
     
